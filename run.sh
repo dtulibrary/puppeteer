5f9adafb3502f8db2f4ddc0f6ac7ce8ce397c472
Jimmy Petersen <jipe@dtu.dk>
2016-10-25 16:41:00 +0200

    Simplify regexp for module path generation

--- run.sh
+++ run.sh
@@ -38,7 +38,7 @@ fi
 # Build puppet modulepath
 for mp in $(ls -d $puppet_home/*); do
   if [[ -d $mp/modules ]]; then
-    mp="/puppet/$(echo $mp | sed -r 's/.*\/[^\/]+\/([^\/]+)$/\1/')/modules"
+    mp="/puppet/$(echo $mp | sed -r 's/.*\/([^\/]+)$/\1/')/modules"
     if [[ -z $module_path ]]; then
       module_path=$mp
     else
