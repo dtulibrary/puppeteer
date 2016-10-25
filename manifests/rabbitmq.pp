5145d9395a353ff91ebd185282ae589ebe73de51
Jimmy Petersen <jipe@dtu.dk>
2016-10-25 16:39:01 +0200

    Removed non-test manifests
    - We're going to ignore manifests contents from now on, since these
      don't belong as part of the project.

--- manifests/rabbitmq.pp
+++
@@ -1,6 +0,0 @@
-node default {
-  class {'metastore::rabbitmq':
-    username => 'rabbit',
-    password => 'rabbit',
-  }
-}
--- manifests/toshokan.pp
+++
@@ -1,9 +0,0 @@
-node default {
-  class {'debian_host':}
-
-  class {'toshokan':
-    rails_env  => 'staging',
-    conf_set   => 'staging',
-    vhost_name => 'unstable.toshokan.docker.io',
-  }
-}
